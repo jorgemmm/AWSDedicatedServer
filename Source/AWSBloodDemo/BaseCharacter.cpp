8b86de36a50866c59eb3d8e9cbc4a12e4de2d815
Jordi <vplasin@gmail.com>
2021-05-15 14:34:30 +0100

    Base Character Class

--- Source/AWSBloodDemo/BaseCharacter.cpp
+++ Source/AWSBloodDemo/BaseCharacter.cpp
@@ -3,18 +3,78 @@
 
 #include "BaseCharacter.h"
 
+//Requires for this actor
+#include "Animation/AnimInstance.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/InputSettings.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+
+#include "MotionControllerComponent.h"
+#include "XRMotionControllerBase.h" // for FXRMotionControllerBase::RightHandSourceId
+
+//Enegine
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+
+//online
+#include "Net/UnrealNetwork.h"
+#include "Engine/Engine.h"
+
+//Utils
+#include  "DrawDebugHelpers.h"
 // Sets default values
 ABaseCharacter::ABaseCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	//Initialize the player's Health
+	MaxHealth = 100.0f;
+	CurrentHealth = MaxHealth;
 
 }
+
+
+
+
+FString GetEnumText(ENetRole Role)
+{
+	switch (Role)
+	{
+	case ROLE_None:
+		return "None";
+	case ROLE_SimulatedProxy:
+		return "SimulatedProxy";
+	case ROLE_AutonomousProxy:
+		return "AutonomousProxy";
+	case ROLE_Authority:
+		return "Authority";
+	default:
+		return "ERROR";
+	}
+}
+
+//Setup Replication
+void ABaseCharacter::GetLifetimeReplicatedProps(TArray <FLifetimeProperty> & OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//Replicate current health.
+	DOREPLIFETIME(ABaseCharacter, CurrentHealth);
+}
+
 
 // Called when the game starts or when spawned
 void ABaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	
 	
 }
 
@@ -22,6 +82,25 @@ void ABaseCharacter::BeginPlay()
 void ABaseCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	AActor* MyOwner = GetOwner();
+	if (MyOwner != nullptr) {
+
+		//Para el jugador
+		DrawDebugString(GetWorld(),
+			MyOwner->GetActorLocation(), //FVector(0, 0, 100), 
+			GetEnumText(MyOwner->GetLocalRole()), this, FColor::White, DeltaTime);
+
+
+
+	}
+	else
+	{
+		//Para enemigos
+		DrawDebugString(GetWorld(),
+			GetActorLocation(), //FVector(0, 0, 100), 
+			GetEnumText(GetLocalRole()), this, FColor::White, DeltaTime);
+	}
 
 }
 
@@ -31,4 +110,108 @@ void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
+
+
+
+void ABaseCharacter::OnRep_CurrentHealth()
+{
+
+	OnHealthUpdate();
+
+
+}
+
+
+void ABaseCharacter::OnHealthUpdate()
+{
+	//Client-specific functionality
+	if (IsLocallyControlled())
+	{
+		FString healthMessage = FString::Printf(TEXT("You now have %f health remaining."), CurrentHealth);
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, healthMessage);
+
+		if (CurrentHealth <= 0)
+		{
+			FString deathMessage = FString::Printf(TEXT("You have been killed."));
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, deathMessage);
+
+			// ragdoll the character
+			//ACharacter* MyCharacter = UGameplayStatics::GetPlayerCharacter(this, 0);
+			//if (MyCharacter)
+			//{
+			   // MyCharacter->GetMesh()->SetSimulatePhysics(true);
+			   // //MyCharacter->GetMovementComponent()->MovementState.bCanJump = false;
+			//}
+		}
+	}
+
+	//Server-specific functionality
+	if (GetLocalRole() == ROLE_Authority)
+	{
+		FString healthMessage = FString::Printf(TEXT("%s now has %f health remaining."), *GetFName().ToString(), CurrentHealth);
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, healthMessage);
+	}
+
+	if (GetLocalRole() == ROLE_AutonomousProxy)
+	{
+		//Suelen ser los propietarios o los que controlan al Pawn
+		//Es decir el Jugador o Hero
+
+	}
+	if (GetLocalRole() == ROLE_SimulatedProxy)
+	{
+		//Suelen ser los enemigos o jugadores o online
+		FString healthMessage = FString::Printf(TEXT("You now have %f health remaining."), CurrentHealth);
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, healthMessage);
+
+		if (CurrentHealth <= 0)
+		{
+			FString deathMessage = FString::Printf(TEXT("You have been killed."));
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, deathMessage);
+
+			// ragdoll the character
+			ACharacter* MyCharacter = UGameplayStatics::GetPlayerCharacter(this, 0);
+			if (MyCharacter)
+			{
+				MyCharacter->GetMesh()->SetSimulatePhysics(true);
+				// //MyCharacter->GetMovementComponent()->MovementState.bCanJump = false;
+			}
+		}
+
+	}
+
+
+	//Functions that occur on all machines. 
+   /*
+
+
+
+	   Any special functionality that should occur as a result of damage or death should be placed here.
+   */
+}
+
+void ABaseCharacter::SetCurrentHealth(float healthValue)
+{
+	//En servidor el valor de health se propagará / replicará
+
+   // GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT(" Set Current Health")));
+	if (GetLocalRole() == ROLE_Authority)
+		//if(HasAuthority())
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Magenta, FString::Printf(TEXT(" Take Damage")));
+		CurrentHealth = FMath::Clamp(healthValue, 0.f, MaxHealth);
+		OnHealthUpdate();
+	}
+}
+
+float ABaseCharacter::TakeDamage(float DamageTaken, FDamageEvent const & DamageEvent, AController * EventInstigator, AActor * DamageCauser)
+{
+	float damageApplied = CurrentHealth - DamageTaken;
+	// GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Orange, FString::Printf(TEXT(" Take Damage")));
+
+	SetCurrentHealth(damageApplied);
+
+	return damageApplied;
+}
+
 
--- Source/AWSBloodDemo/BaseCharacter.h
+++ Source/AWSBloodDemo/BaseCharacter.h
@@ -26,4 +26,37 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	/** The player's maximum health. This is the highest that their health can be, and the value that their health starts at when spawned.*/
+	UPROPERTY(EditDefaultsOnly, Category = "Health")
+		float MaxHealth;
+
+	/** The player's current health. When reduced to 0, they are considered dead.*/
+	UPROPERTY(ReplicatedUsing = OnRep_CurrentHealth)
+		float CurrentHealth;
+
+	/** RepNotify for changes made to current health.*/
+	UFUNCTION()
+		void OnRep_CurrentHealth();
+
+
+
+
+	/** Response to health being updated. Called on the server immediately after modification, and on clients in response to a RepNotify*/
+	void OnHealthUpdate();
+
+	/** Getter for Max Health.*/
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE float GetMaxHealth() const { return MaxHealth; }
+
+	/** Getter for Current Health.*/
+	UFUNCTION(BlueprintPure, Category = "Health")
+		FORCEINLINE float GetCurrentHealth() const { return CurrentHealth; }
+
+	/** Setter for Current Health. Clamps the value between 0 and MaxHealth and calls OnHealthUpdate. Should only be called on the server.*/
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		void SetCurrentHealth(float healthValue);
+
+	/** Event for taking damage. Overridden from APawn.*/
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		float TakeDamage(float DamageTaken, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
 };
